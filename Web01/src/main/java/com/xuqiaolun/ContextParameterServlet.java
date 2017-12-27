ee276ff9ee56419aabf8058e6ea4edb724f211ec
Xuqiaolun <Jamesxql@Gmail.com>
2017-12-27 19:07:40 +0800

    add some project

---
+++ Web01/src/main/java/com/xuqiaolun/ContextParameterServlet.java
@@ -0,0 +1,24 @@
+package com.xuqiaolun;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "contextParameterServlet", urlPatterns = {"/contextParameter"})
+public class ContextParameterServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException
+    {
+        ServletContext c = this.getServletContext();
+        PrintWriter writer = response.getWriter();
+
+        writer.append("settingOne: ").append(c.getInitParameter("settingOne"))
+                .append("settingTwo: ").append(c.getInitParameter("settingTwo"));
+    }
+}
